55e48f31a25ddebfee40a51359fbb30796587dc4
lindacelmaa <lindacelma98@gmail.com>
2025-03-21 09:18:40 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/JavaSeminar06Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSeminar06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSeminar06Application.class, args);
+	}
+
+}
